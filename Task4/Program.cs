1f239c58e156477be8f5d27cfbe5766421cd18c8
“iamIlyaAlex” <blagas77@me.com>
2023-12-27 11:03:04 +0300

    added less1/Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 8;
+int b = 32;
+int c = 14;
+int d = 22;
+int e = 2;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
