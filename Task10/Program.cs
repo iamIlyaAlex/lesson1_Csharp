bb516f2175846264513079b5fa5bbe384e55af40
“iamIlyaAlex” <blagas77@me.com>
2023-12-31 14:18:46 +0300

    added Task10/Less1

---
+++ Task10/Program.cs
@@ -0,0 +1,50 @@
+﻿// разбор 1 домашнего задания
+
+// int a = 5;
+// int b = 6;
+
+//1
+// Console.Write("Первое число ");
+// Console.Write("`");
+// Console.Write(a);
+// Console.Write("`");
+// Console.Write(" меньше чем второе число ");
+// Console.Write("`");
+// Console.Write(b);
+// Console.Write("`");
+
+//2
+// Console.Write($"Первое число `{a}` меньше чем второе число `{b}`");
+
+//3
+// Console.Write("Первое число `"
+// + a
+// + "` меньше чем второе число `"
+// + b
+// + "`");
+
+//4 return
+// {
+//     int res = some_test(4, 6, 1);
+//     Console.WriteLine(res);
+// }
+
+//     int some_test(int a, int b, int c)
+// { // Функция с 3 параметрами
+//     return a + b + c; // Функция возвращает результат суммы чисел
+// }
+
+//5 return
+// int Sum(int x, int y)
+// {
+//     return x + y;
+// }
+
+// int result = Sum(10, 15);   // 25
+// Console.WriteLine(result);   // 25
+
+// Console.WriteLine(Sum(5, 6));   // 11
+
+// приминение "\t"
+// int i = 4;
+// Console.WriteLine(i +"\t");
